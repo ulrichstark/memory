27e95eed2ac90189dc70f7fd722141c4b5fbba4f
Ulrich Stark <ulrich.stark@gmx.de>
2020-06-20 15:19:47 +0200

    added winner feature

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ let secondsPassed = 0;
 let activePlayer = 0;
 let clockHandle = 0;
 let lastCard = null;
+let foundPairs = 0;
 
 const settingsPlayers = document.getElementById("settings-players");
 const settingsAddPlayer = document.getElementById("settings-add-player");
@@ -265,6 +266,9 @@ function createPlayerList() {
         const playerListItem = document.createElement("li");
         playerListItem.classList.add("player");
 
+        playerListItem.innerHTML =
+            '<svg class="player-crown" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z"/></svg>';
+
         const playerName = document.createElement("span");
         playerName.classList.add("player-name");
 
@@ -290,6 +294,8 @@ function createPlayerList() {
                 playerListItem.classList.remove("active");
             }
         };
+
+        player.setWinner = () => playerListItem.classList.add("won");
 
         player.updateSmileys();
         player.updateTries();
@@ -335,6 +341,22 @@ function setBoardClickable(clickable) {
     elementBoard.style.pointerEvents = clickable ? "auto" : "none";
 }
 
+function updateWinners() {
+    let highestScore = 0;
+
+    for (const player of players) {
+        if (player.smileys.length > highestScore) {
+            highestScore = player.smileys.length;
+        }
+    }
+
+    for (const player of players) {
+        if (player.smileys.length === highestScore) {
+            player.setWinner();
+        }
+    }
+}
+
 function resolveCardPair(card) {
     const player = players[activePlayer];
     player.tries++;
@@ -347,6 +369,15 @@ function resolveCardPair(card) {
 
         player.smileys.push(card.smiley);
         player.updateSmileys();
+
+        foundPairs++;
+
+        if (foundPairs === pairs) {
+            activePlayer = null;
+            updateActivePlayer();
+
+            updateWinners();
+        }
     } else {
         lastCard.flip();
         card.flip();
@@ -363,6 +394,7 @@ function startGame() {
         return;
     }
 
+    foundPairs = 0;
     lastCard = null;
 
     preparePlayersForGame();
